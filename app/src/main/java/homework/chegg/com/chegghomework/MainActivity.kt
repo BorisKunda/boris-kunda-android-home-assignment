c219e04afd70643ba60703e437816974ba462cc8
Boris Kunda <34305099+BorisKunda@users.noreply.github.com>
2021-03-14 14:07:11 +0200

    Initial commit

---
+++ app/src/main/java/homework/chegg/com/chegghomework/MainActivity.kt
@@ -0,0 +1,50 @@
+package homework.chegg.com.chegghomework
+
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.view.View
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import androidx.recyclerview.widget.RecyclerView
+
+class MainActivity : AppCompatActivity() {
+    private var toolbar: Toolbar? = null
+    private var mRecyclerView: RecyclerView? = null
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        buildUI()
+    }
+
+    private fun setupToolbar() {
+        toolbar = findViewById<View>(R.id.toolbar) as Toolbar
+        setSupportActionBar(toolbar)
+    }
+
+    private fun buildUI() {
+        setContentView(R.layout.activity_main)
+        setupToolbar()
+        mRecyclerView = findViewById<View>(R.id.my_recycler_view) as RecyclerView
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.menu_main_activity, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.action_refresh -> {
+                onRefreshData()
+                return true
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    // TODO fetch data from all data sources, aggregate data and display in RecyclerView
+    private fun onRefreshData() {
+        Toast.makeText(this, "Fetch data aggregate and show on RecyclerView", Toast.LENGTH_SHORT).show()
+    }
+}
